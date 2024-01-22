78280ebc159a4aa509452aee84a32ac3b68fedc3
gonchar <s@gonchar.me>
2024-01-22 22:11:59 +0000

    add vision

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -9,7 +9,8 @@ let package = Package(
         .macOS(.v10_15),
         .iOS(.v12),
         .tvOS(.v12),
-        .watchOS(.v5)
+        .watchOS(.v5),
+        .visionOS(.v1)
     ],
     products: [
         .library(
